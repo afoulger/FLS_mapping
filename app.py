b106ece3d76b2f85e441d685a7c7436dae5aed8f
afoulger <acfoulger29@gmail.com>
2026-02-20 09:46:49 +0000

    Add more datasets

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import re
 
 st.title("My FLS mapping app")
 
-def load_data(df_name, skiprows, usecols):
+def load_dexa_data(df_name, skiprows, usecols):
    
        #Load data 
        df = grab_clean_df(df_name, skiprows, usecols)
@@ -20,7 +20,7 @@ def load_data(df_name, skiprows, usecols):
 
        return df
 
-def grab_clean_df(df_name, skiprows, usecols):
+def grab_clean_df(df_name, skiprows=None, usecols=None):
        #Load csv files and tidy up column names
        df = pd.read_csv(df_name, skiprows=skiprows, usecols=usecols)
        df = df.rename(columns=lambda x: tokenize(x))
@@ -41,10 +41,14 @@ def tokenize(text):
 dexa_data_cols = ['Region Code', 'Region', 'ICB - Integrated Care Board',
        'Imaging Network', 'Organisation Code', 'Organisation Name',' DEXA ']
 
-#Load dexa data
-dexa_data_2223 = load_data('Data/National-Imaging-Data-Collection-Asset-Count-2022-23-v1.csv', skiprows=13, usecols=dexa_data_cols)  
-dexa_data_2324 = load_data('Data/National-Imaging-Data-Collection-Asset-Count-2023-24-v1.csv', skiprows=13, usecols=dexa_data_cols)  
-dexa_data_2425 = load_data('Data/National-Imaging-Data-Collection-Asset-Count-2024-25-v1-FINAL.csv', skiprows=13, usecols=dexa_data_cols)  
+#Load data
+dexa_data_2223 = load_dexa_data('Data/National-Imaging-Data-Collection-Asset-Count-2022-23-v1.csv', skiprows=13, usecols=dexa_data_cols)  
+dexa_data_2324 = load_dexa_data('Data/National-Imaging-Data-Collection-Asset-Count-2023-24-v1.csv', skiprows=13, usecols=dexa_data_cols)  
+dexa_data_2425 = load_dexa_data('Data/National-Imaging-Data-Collection-Asset-Count-2024-25-v1-FINAL.csv', skiprows=13, usecols=dexa_data_cols)  
+
+fls_list_2024 = grab_clean_df('Data/FLS_List_2024.csv')
+neighbourhood_health_areas = grab_clean_df('Data/Neighbourhood_Health_areas.csv')
+nhs_trusts = grab_clean_df('Data/NHS_Trusts.csv')
 
 #Display datasets on streamlit
 st.write("DEXA scanner data 2022-23")
@@ -55,6 +59,15 @@ dexa_data_2324
 
 st.write("DEXA scanner data 2024-25")
 dexa_data_2425
+
+st.write("FLS List 2024")
+fls_list_2024
+
+st.write("Neighbourhood Health areas")
+neighbourhood_health_areas
+
+st.write("NHS Trusts")
+nhs_trusts
 
 #Count number of dexa scanners in each year
 dexa_count_2223 = dexa_data_2223['dexa_count'].sum()
